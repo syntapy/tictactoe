0c8ebc855e9810de0f217740d3731b9f7e4f5442
Sami Abdul-Wahid <syntapy@pm.me>
2022-07-19 17:59:36 -0700

    blank grid displays with correct borders

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,42 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 
-interface top {
-  top: any,
-}
-
-interface bottom {
-  bottom: any,
-}
-
-interface left {
-  left: any,
-}
-
-interface right {
-  right: any,
-}
-
-interface haschildren {
+interface SquareProps {
+  top: boolean
+  bottom: boolean
+  left: boolean
+  right: boolean
+  key: number
   children?: string
 }
 
-export class Square extends React.Component<haschildren & (top | bottom | left | right)> {
+class Square extends React.Component<SquareProps, any> {
 
-  constructor(props: any) {
+  constructor(props: SquareProps) {
     super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e: any) {
   }
 
   render() {
     let className: string = "aspect-square border-black p-0 m-0 font-sans text-center width-full height-full "
 
-    if (this.props.hasOwnProperty("top")) {
+    if (this.props.top) {
       className += "border-t-2 " 
     }
 
-    if (this.props.hasOwnProperty("bottom")) {
-      className += "border-b-2 "
-    }
-
-    if (this.props.hasOwnProperty("left")) {
+    if (this.props.left) {
       className += "border-l-2 "
     }
 
-    if (this.props.hasOwnProperty("right")) {
+    if (this.props.bottom) {
+      className += "border-b-2 "
+    }
+
+    if (this.props.right) {
       className += "border-r-2 "
     }
 
@@ -52,21 +46,44 @@ export class Square extends React.Component<haschildren & (top | bottom | left |
     }
 
     return (
-      <div className={className}>{value}</div>
+      <div onClick={this.handleClick} className={className}>{value}</div>
     )
   }
 }
 
-export class Board extends React.Component<any, any> {
+interface SquareContainer {
+  component: Square,
+  info: {value: string}
+}
+
+interface BoardState {
+  squares: Array<SquareContainer>,
+}
+
+export class Board extends React.Component<any, BoardState> {
 
   constructor(props: any) {
     super(props)
     this.state={
-      squares: Array(9).fill(null)
+      squares: (new Array(9)).fill({component: null, info: {value: ""}}),
+    }
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const b: boolean = (row === 2)
+        const r: boolean = (col === 2)
+        const square: SquareContainer = {
+          component: new Square({
+            top: true, bottom: b, left: true, right: r, key: row*3 + col, children: ""
+          }),
+          info: {value: ""},
+        }
+        this.state.squares[row*3 + col] = square
+      }
     }
   }
 
-  clickSquare(row: number, col: number, val: string) {
+  clickSquare(row: number, col: number, val: any) {
     if (row < 0 || row > 2 || col < 0 || col > 2) {
       throw new Error("Invalid row or column")
     }
@@ -77,17 +94,9 @@ export class Board extends React.Component<any, any> {
   render() {
     return (
       <div className="w-1/5 aspect-square border-black border-x-0 border-y-0 grid grid-cols-3">
-        <Square top left>X</Square>
-        <Square top left>O</Square>
-        <Square top left right>X</Square>
-
-        <Square top left>O</Square>
-        <Square top left>X</Square>
-        <Square top left right>O</Square>
-
-        <Square top left bottom>X</Square>
-        <Square top left bottom>O</Square>
-        <Square top left right bottom>X</Square>
+        {this.state.squares.map((squareinfo: SquareContainer, index: number) => { 
+          return squareinfo.component.render() 
+        })}
       </div>
     );
   }
