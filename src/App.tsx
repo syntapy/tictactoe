a78796df034b592cda9e90de4ea09967a9e726f7
Sami Abdul-Wahid <syntapy@pm.me>
2022-07-23 15:33:50 -0700

    get rid of dynamic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 
-const NUM_ROWS_COLS: number = 4
-
-class GameState {
-  squares: Array<SquareContainer>
-  argsquares: [number, number][] = []
-  symbols: [string, string] = ['X', 'O']
-  turn: number = -1
-
-  constructor() {
-    this.squares = new Array(Math.pow(NUM_ROWS_COLS, 2)).fill({component: null, info: {value: ""}})
-  }
-
-  pushSymbol(): string {
-    this.turn = (this.turn + 1) % 2
-    return this.symbols[this.turn]
-  }
-
-  pushSquare(row: number, col: number): string {
-    if (row < 0 || col < 0 || row > NUM_ROWS_COLS-1 || col > NUM_ROWS_COLS-1) {
-      throw("Invalid row or column")
-    }
-    let symbol: string = this.pushSymbol()
-    this.argsquares.push([row, col])
-    this.squares[NUM_ROWS_COLS*row + col].info.value = symbol
-
-    return symbol
-  }
-
-  popSquare() {
-    let tuple: [number, number] | undefined = this.argsquares.pop()
-    if (tuple) {
-      let row: number = tuple[0]
-      let col: number = tuple[1]
-      if (row < 0 || col < 0 || row > NUM_ROWS_COLS-1 || col > NUM_ROWS_COLS-1) {
-        throw new Error("Invalid square")
-      }
-      this.squares[NUM_ROWS_COLS*row + col].info.value = ""
-    } else {
-      throw new Error("No square to pop")
-    }
-  }
-
-  isArgWinner(val: string): boolean {
-    let count_horiz: number = 0
-    let count_vert: number = 0
-    let count_diag_left: number = 0
-    let count_diag_right: number = 0
-
-    let row: number, col: number
-
-    // Check horizontal and vertical
-    for (let i = 0; i < NUM_ROWS_COLS; i++) {
-      for (let j = 0; j < NUM_ROWS_COLS; j++) {
-        row = i
-        col = j
-        if (this.squares[row * NUM_ROWS_COLS + col].info.value === val) {
-          count_horiz++
-        }
-
-        row = j
-        col = i
-        if (this.squares[row * NUM_ROWS_COLS + col].info.value === val) {
-          count_vert++
-        }
-      }
-    }
-
-    // Check diagonals
-    for (let i = 0; i < NUM_ROWS_COLS; i++) {
-      row = i
-      col = i
-      if (this.squares[row * NUM_ROWS_COLS + col].info.value === val) {
-        count_diag_left++
-      }
-
-      row = NUM_ROWS_COLS-1 - i
-      if (this.squares[row * NUM_ROWS_COLS + col].info.value === val) {
-        count_diag_right++
-      }
-    }
-
-    if (count_horiz === NUM_ROWS_COLS || count_vert === NUM_ROWS_COLS || count_diag_left === NUM_ROWS_COLS || count_diag_right === NUM_ROWS_COLS) {
-      return true
-    }
-
-    return false
-  }
-}
+const NUM_ROWS_COLS: number = 3
 
 interface SquareProps {
   top: boolean
@@ -97,7 +10,6 @@ interface SquareProps {
   right: boolean
   row: number
   col: number
-  gameState: GameState
   parent: Board
   key: number
 }
@@ -107,7 +19,6 @@ interface SquareState {
 }
 
 interface BoardState {
-  gameState: GameState
   squares: Array<SquareContainer>
 }
 
@@ -121,21 +32,9 @@ class Square extends React.Component<SquareProps, SquareState> {
   constructor(props: SquareProps) {
     super(props)
 
-    this.handleClick = this.handleClick.bind(this)
-
     this.state = {children: ""}
 
     this.setState(this.state)
-  }
-
-  handleClick(e: any) {
-    let state: any = this.state
-    if (state.children === "") {
-      let symbol: string = this.props.gameState.pushSquare(this.props.row, this.props.col)
-      state.children = symbol
-      this.props.parent.forceUpdate()
-      this.setState(state)
-    }
   }
 
   render() {
@@ -163,7 +62,7 @@ class Square extends React.Component<SquareProps, SquareState> {
     }
 
     return (
-      <div onClick={this.handleClick} className={className}>{value}</div>
+      <div className={className}>{value}</div>
     )
   }
 }
@@ -172,9 +71,7 @@ export class Board extends React.Component<{}, BoardState> {
 
   constructor(props: {}) {
     super(props)
-    const gameState: GameState = new GameState()
     let state: BoardState = {
-      gameState: gameState,
       squares: new Array(Math.pow(NUM_ROWS_COLS, 2)).fill({component: null, info: {value: ""}}),
     }
 
@@ -185,7 +82,7 @@ export class Board extends React.Component<{}, BoardState> {
         const square: SquareContainer = {
           component: new Square({
             top: true, left: true, bottom: b, right: r, row: row, col: col,
-            gameState: state.gameState, parent: this, key: NUM_ROWS_COLS*row + col,
+            parent: this, key: NUM_ROWS_COLS*row + col,
           }),
           info: {value: ""},
         }
