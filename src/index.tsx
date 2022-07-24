dab8273e4a0125809ab3d9260981553b39ea95f6
Sami Abdul-Wahid <syntapy@pm.me>
2022-07-24 02:42:14 -0700

    rm serviceWorker and setupTest

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import "./styles/tailwind.css";
 import Board from "./App";
-import * as serviceWorker from "./serviceWorker";
 import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -9,8 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Board />
   </React.StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
